60d2c14999b303114f5add0f662234892cc12328
yakov matan <yakovmatan@gmail.com>
2025-06-25 10:08:18 +0300

    add function __eq__

--- Shape.py
+++ Shape.py
@@ -1,5 +1,8 @@
 from abc import ABC,abstractmethod
+from functools import total_ordering
 
+
+@total_ordering
 class Shape(ABC):
 
     @abstractmethod
@@ -15,3 +18,9 @@ class Shape(ABC):
 
     def __repr__(self):
         return f"<{self.__class__.__name__} object>"
+
+    def __eq__(self, other):
+        if not isinstance(other,Shape):
+            return False
+        return self.get_area() == other.get_area()
+
